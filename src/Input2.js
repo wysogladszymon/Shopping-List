22d095eff1483faa0c5a1baa525abc1c0fb60f9b
Szymon <wysogladszymon@gmail.com>
2023-11-24 00:29:20 +0100

    end for now

--- src/Input2.js
+++ src/Input2.js
@@ -2,8 +2,8 @@ import './Input2.css'
 
 export default function Input2(){
     return <div className='input-container'>
-        <input className='inputs input-product' id='input-product-id' placeholder='product'></input>
-        <input className='inputs input-amount' placeholder='amount'></input>
-        <div className='check-mark'>&#9989;</div>
+        <input className='inputs input-product' id='input-product-id' placeholder='product' pattern="[A-Za-z0-9]+"></input>
+        <input className='inputs input-amount' placeholder='amount' pattern="[0-9]+"></input>
+        {/* <div className='check-mark'>&#9989;</div> */}
     </div>
 }
